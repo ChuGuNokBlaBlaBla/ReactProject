dd4c6e83ccf6968033b6a0da661f5324185b6bfa
Иван Старов <ivanAndreevichStarov@yandex.ru>
2023-09-12 19:01:42 +0200

    удалил коментарий из index.js

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ const renderDOM = (dataApp) => {
     </React.StrictMode>
   );
 }
-// comment
+
 renderDOM(store.getState())
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
